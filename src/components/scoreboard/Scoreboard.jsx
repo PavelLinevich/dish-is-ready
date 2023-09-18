a9ae57361aeba3bfc75c37d792f65bb81d17c3dd
PavelGith <linevich.p.l@gmail.com>
2023-09-18 15:29:19 +0300

    feat: remove Menu component

--- src/components/scoreboard/Scoreboard.jsx
+++ src/components/scoreboard/Scoreboard.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
 import './Scoreboard.css'
 
 function Scoreboard() {
@@ -45,21 +43,19 @@ function Scoreboard() {
     }
   }
 
-  const [anchorEl, setAnchorEl] = useState(null)
-  const open = anchorEl
-  const changeOrderStatusClick = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = (e) => {
-    setAnchorEl(null)
-    console.log(e);
-  }
-
   return (
     <div>
       <div style={{ padding: '10px', display: 'flex', }}>
+        <Button
+          onClick={createOrderClick}
+          style={{ marginRight: '10px' }}
+          variant='contained'
+          color='error'
+        >
+          Create order
+        </Button>
         <TextField
-          label='Add order number'
+          label={'№ ' + lastOrderNumber + ' or Add order number'}
           variant='standard'
           onChange={handleChange}
           onKeyDown={onKeyPressHandler}
@@ -67,42 +63,18 @@ function Scoreboard() {
           className='createOrder'
           type='number'
         />
-        <Button
-          onClick={createOrderClick}
-          style={{ marginLeft: '10px' }}
-          variant='contained'
-          color='error'
-        >
-          Create order
-        </Button>
       </div>
       <div className='scoreboard'>
         <div className='leftScoreboard'>
           {orders.filter(order => order.status === 'open').map((element) => {
             return (
-              <div>
                 <Button
                   key={element.id}
                   variant='contained'
-                  id="basic-button"
-                  aria-controls={open ? 'basic-menu' : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? 'true' : undefined}
-                  onClick={changeOrderStatusClick}
+
                 >
                   <p>{element.id}</p>
                 </Button>
-                <Menu
-                  id="basic-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{ 'aria-labelledby': 'basic-button' }}
-                >
-                  <MenuItem onClick={handleClose}>Order ready</MenuItem>
-                  <MenuItem onClick={handleClose}>Order close</MenuItem>
-                </Menu>
-              </div>
             )
           })}
         </div>
@@ -112,7 +84,6 @@ function Scoreboard() {
               <Button
                 key={element.id}
                 variant='contained'
-                onClick={changeOrderStatusClick}
               >
                 <p>{element.id}</p>
               </Button>
